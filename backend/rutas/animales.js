1d7b7b9cabe11f5841b05dc3e5bed1f664b342af
acostaisma03-cmyk <acostaisma03@gmail.com>
2026-09-17 15:09:38 -0300

    ruta animales

--- backend/rutas/animales.js
+++ backend/rutas/animales.js
@@ -13,4 +13,25 @@ router.post("/", async(req, res) => {
         fecha_nacimiento,
         estado_actual
     } = req.body;
+    try {
+        const[resultado] = await pool.execute(
+           `INSERT INTO animales (especie,raza,nombre,sexo,fecha_nacimiento,estado_actual) VALUES (?,?,?,?,?,?)`,
+           [
+              nombre,
+              especie,
+              raza,
+              sexo,
+              fecha_nacimiento,
+              estado_actual
+           ]
+        )
+        return res.status(201).json({mensaje:"animales agregado correctamente"
+       } )
+    }catch (error) {
+        return res.status(500).json({
+            error:"No se pudo agregar el producto",
+            detalle:error.message
+    })
+    }
+
 })
